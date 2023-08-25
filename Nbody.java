abbb8cd4e5080fe8169bbb40ba7e0dd4808615a8
nxthyn <nathanh2362\@lpsk12.org>
2023-08-25 11:08:52 -0600

    Created java files 'Planet.java' and 'NBody.java'

---
+++ Nbody.java
@@ -0,0 +1,9 @@
+public class Nbody {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ Planet.java
@@ -0,0 +1,9 @@
+public class Planet {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
