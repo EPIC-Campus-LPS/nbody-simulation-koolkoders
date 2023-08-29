1a546b69384b89eacf38e86715d27be16d892568
mariev718_lpsk12 <mariev718@lpsk12.org>
2023-08-29 10:04:59 -0600

    Planet.java PIV and constructor

--- NBody.java
+++ NBody.java
@@ -1,2 +1,11 @@
 public class NBody {
+
+    private double duration;
+    private double tIncrement;
+
+    public NBody(double d, double t) {
+        duration = d;
+        tIncrement = t;
+    }
+
 }
--- Planet.java
+++ Planet.java
@@ -1,8 +1,33 @@
+import java.math.BigInteger;
+
 public class Planet {
 
-    public static void main(String[] args) {
+    // x-coordinate of planet
+    private BigInteger xCoord;
 
+    // y-coordinate of planet
+    private BigInteger yCoord;
 
+    // x-component of initial velocity
+    private BigInteger xVelocity;
+
+    // y-component of initial velocity
+    private BigInteger yVelocity;
+
+    // mass of planet
+    private BigInteger mass;
+
+    // filename of planet image
+    private String image;
+
+    public Planet (BigInteger xC, BigInteger yC, BigInteger xV, BigInteger yV, BigInteger m, String i) {
+
+        xCoord = xC;
+        yCoord = yC;
+        xVelocity = xV;
+        yVelocity = yV;
+        mass = m;
+        image = i;
 
     }
 
