d04a936d260198d4151b8b92ffd9a76aec3f2d16
mariev718_lpsk12 <mariev718@lpsk12.org>
2023-08-29 11:11:23 -0600

    NBody.java starting scanner thingy

--- NBody.java
+++ NBody.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.io.File;
 public class NBody {
 
     private double duration;
@@ -6,6 +8,18 @@ public class NBody {
     public NBody(double d, double t) {
         duration = d;
         tIncrement = t;
+    }
+
+    public static void main (String[] args) throws Exception {
+
+        File file = new File("planets.txt");
+
+        Scanner input = new Scanner(file);
+
+        int bodies = 0;
+
+        Planet[] game = new Planet[bodies];
+
     }
 
 }
